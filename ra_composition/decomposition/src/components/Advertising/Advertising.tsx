70550175c1d7c800379399889e4776509c87d42d
SubHunt <phaeton.kz@gmail.com>
2024-09-13 13:55:03 +0300

    ra_composition

---
+++ ra_composition/decomposition/src/components/Advertising/Advertising.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ * Рекламный баннер
+ */
+
+export function Advertising() {
+
+    return (
+        <img  className='banner'
+            src='https://avatars.mds.yandex.net/i?id=f700a3aa05ee862ea30a1daab5f85c94_l-10754985-images-thumbs&n=13'
+            alt=''
+            style={{ width: '600px', height: '200px' }}
+        />
+    );
+}
---
+++ ra_composition/decomposition/src/types.ts
@@ -0,0 +1,23 @@
+export type TNews = {
+    img: string,
+    title: string,
+    link: string,
+}
+
+export type TCategories = string[];
+
+export type TVisited = {
+    title: string,
+    description: string,
+};
+
+export type TMapGermany = string[];
+
+export type TTVguide = {
+    time: string,
+    name: string,
+};
+
+
+
+export type TOnline = string[];
